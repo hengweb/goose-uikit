8471ed6e223611e02514832735562908e9b41f50
hengweb <81994389+hengweb@users.noreply.github.com>
2021-07-17 14:36:44 +0700

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2856,13 +2856,13 @@ var baseColors = {
     primaryBright: "#00b10e",
     primaryDark: "#00b10e",
     secondary: "#6d1d1d",
-    success: "#31D0AA",
+    success: "#08e800",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#d7e9d9", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#6d1d1d", textDisabled: "#BDC2C4", textSubtle: "#6d1d1d", borderColor: "#E9EAEB", card: "#e2fbe4", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#d7e9d9", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#6d1d1d", tertiary: "#EFF4F5", text: "#6d1d1d", textDisabled: "#BDC2C4", textSubtle: "#6d1d1d", borderColor: "#E9EAEB", card: "#e2fbe4", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#9A6AFF", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
